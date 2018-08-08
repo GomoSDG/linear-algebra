409aa758fa7920cf933ef99aa90d081f5a4d9f2e
gomosdg <lilokoego@outlook.com>
2018-08-08 14:13:29 +0200

    added some operations and notes

--- main.py
+++ main.py
@@ -4,15 +4,48 @@ if __name__ == "__main__":
     # Addition
     v1 = Vector([8.218, -9.341])
     v2 = Vector([-1.129, 2.111])
+    print('{} + {} = {}'.format(v1, v2, v1 + v2))
+    print()
 
     # Subtraction
     v3 = Vector([7.119, 8.215])
     v4 = Vector([-8.223, 0.878])
+    print('{} - {} = {}'.format(v3, v4, v3 - v4))
+    print()
 
     # Multiplication
     v5 = 7.41
     v6 = Vector([1.671, -1.012, -0.318])
-
-    print('{} + {} = {}'.format(v1, v2, v1 + v2))
-    print('{} - {} = {}'.format(v3, v4, v3 - v4))
     print('{} * {} = {}'.format(v5, v6, v5 * v6))
+    print()
+
+    # Magnitude
+    v7 = Vector([-0.221, 7.437])
+    v8 = Vector([8.813, -1.331, -6.247])
+    print('||{}|| = {}'.format(v7, v7.magnitude()))
+    print('||{}|| = {}'.format(v8, v8.magnitude()))
+    print()
+
+    # Normalisation
+    v9 = Vector([5.581, -2.136])
+    v10 = Vector([1.996, 3.108, -4.554])
+    print('1/||{}|| * {} = {}'.format(v9, v9, v9.normalise()))
+    print('1/||{}|| * {} = {}'.format(v10, v10, v10.normalise()))
+    print()
+
+    # Dot Product
+    v11 = Vector([7.887, 4.138])
+    v12 = Vector([-8.802, 6.776])
+    v13 = Vector([-5.955, -4.904, -1.874])
+    v14 = Vector([-4.496, -8.755, 7.103])
+    print('{} * {} = {}'.format(v11, v12, v11 * v12))
+    print('{} * {} = {}'.format(v13, v14, v13 * v14))
+    print()
+
+    # Angle Between
+    v15 = Vector([3.183, -7.627])
+    v16 = Vector([-2.668, 5.319])
+    v17 = Vector([7.35, 0.221, 5.188])
+    v18 = Vector([2.751, 8.259, 3.985])
+    print('Angle between {} and {} = {}'.format(v15, v16, v15.get_angle_with(v16)))
+    print('Angle between {} and {} = {}'.format(v17, v18, v17.get_angle_with(v18, True)))
--- vector.py
+++ vector.py
@@ -1,5 +1,8 @@
 import math
+from utils import cross_product
 
+
+# TODO: Implement TypeErrorss
 class Vector(object):
     def __init__(self, coordinates):
         try:
@@ -18,22 +21,92 @@ class Vector(object):
         return 'Vector: {}'.format(self.coordinates)
 
     def __eq__(self, v2):
+        """
+        Checks if two vectors are equal
+        :param v2: second vector.
+        :return: boolean
+        """
         return self.coordinates == v2.coordinates
 
     def __add__(self, v2):
         return tuple(map(lambda x: x[1] + v2[x[0]], enumerate(self)))
 
     def __sub__(self, v2):
+        """
+        Performs subtraction of vectors.
+        :param v2: second vector.
+        :return: returns sum of the two vectors type vector.
+        """
         return tuple(map(lambda x: x[1] - v2[x[0]], enumerate(self)))
 
     def __iter__(self):
         return iter(self.coordinates)
 
     def __mul__(self, other):
+        """
+        Multiplication of vectors.
+        :param other: second operand.
+        :return: Product of multiplication (may be scalar or vector).
+        """
         if isinstance(other, float) or isinstance(other, int):
             return tuple(map(lambda x: x * other, self))
 
-        raise NotImplemented
+        # ||v1||.||v2||.cosO
+        # v1.v2 = v1,1 * v2,1 + ... + v1,n * v2,n
+        # Let's us find angle between 2 angles.
+        if isinstance(other, Vector):
+            return sum([x * y for x, y in zip(self, other)])
+
+    def is_parallel_to(self, v2):
+        """
+        Parallel if in the same direction or opposite direction.
+        :param v2:
+        :return:
+        """
+        return (self.is_zero() or v2.is_zero()
+                or self.get_angle_with(v2, True) == 0
+                or self.get_angle_with(v2, True) == 180)
+
+    def is_othogonal_to(self, v2):
+        """
+
+        :param v2:
+        :return:
+        """
+        pass
+
+    def project_onto(self, b):
+        """
+        Project this vector onto the basis provided
+        :param v: The basis
+        :return: Vector projection of this onto b.
+        """
+        pass
+
+    def is_zero(self):
+        return self.magnitude() == 0
+
+    def get_angle_with(self, v2, in_degrees=False):
+        """
+        Angle between self and v2.
+        :param in_degrees:
+        :param v2:
+        :return:
+        """
+        # Derived from Dot Product formulae.
+        # return math.acos((self * v2) / (self.magnitude() * v2.magnitude()))
+        # Couch-Schwarz Inequality
+        # - |v1 * v2| <= ||v1|| * ||v2|| NB!!!
+        # - Assuming neither v1 and v2 are 0
+        # -- if v1 * v2 = ||v1|| * ||v2|| then cosO = 1 (O = 0) and v1 and v2 in same direction.
+        # -- if v1 * v2 = -(||v1|| * ||v2||) then cosO = -1 (O = 180) and v1 and v2 in opposite direction.
+        # -- if v1 * v2 = 0 then cosO = 0 (O = 90) and v1 and v2 form right angle
+        # - v1 * v1 = ||v1|| ** 2 therefore ||v1||=sqrt(v1 * v1)
+        # - since O = 0.
+        if not in_degrees:
+            return math.acos(self.normalise() * v2.normalise())
+        else:
+            return math.degrees(self.get_angle_with(v2))
 
     def __rmul__(self, other):
         return self * other
@@ -42,16 +115,40 @@ class Vector(object):
         return self.coordinates[item]
 
     def magnitude(self):
+        """
+        Calculates the magnitude or length of the vector.
+        :return: Magnitude (scalar).
+        """
         return math.sqrt(sum(map(lambda x: x ** 2, self)))
 
     def __len__(self):
         return len(self.coordinates)
 
     def normalise(self):
+        """
+        Converts current vector to magnitude 1.
+        :return: A unit vector (Direction).
+        """
         if self.is_unit_vector():
+            print('unit!')
             return Vector(self)
 
-        return Vector((1 / self.magnitude()) * self)
+        try:
+            return Vector((1 / self.magnitude()) * self)
+
+        except ZeroDivisionError:
+            raise Exception('Cannot normalise the zero vector')
 
     def is_unit_vector(self):
+        """
+        Checks if current vector is unit vector.
+        :return: boolean.
+        """
         return self.magnitude() == 1
+
+    def is_zero_vector(self):
+        """
+        Checks if current vector is unit vector.
+        :return: boolean
+        """
+        return self.magnitude() == 0
